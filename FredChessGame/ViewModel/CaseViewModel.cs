527a93fb83c732d9824101c70d04169034c812e6
Freddy Juhel <domaine75@gmail.com>
2025-07-28 17:01:48 +0200

    add move

--- FredChessGame/ViewModel/CaseViewModel.cs
+++ FredChessGame/ViewModel/CaseViewModel.cs
@@ -1,39 +1,92 @@
 ﻿using System.ComponentModel;
+using System.Windows.Media;
 using FredChessGame.Model;
 
 namespace FredChessGame.ViewModel
 {
-  public class CaseViewModel: INotifyPropertyChanged
-  {
-    public int Ligne { get; }
-    
-    public int Colonne { get; }
-    
-    private Piece _piece;
-    public (int Ligne, int Colonne) Position => (Ligne, Colonne);
-
-    public Piece Piece
+    public class CaseViewModel : INotifyPropertyChanged
     {
-      get => _piece;
-      set
-      {
-        _piece = value;
-        OnPropertyChanged(nameof(Piece));
-        OnPropertyChanged(nameof(Symbole));
-      }
+        public int Ligne { get; }
+        public int Colonne { get; }
+        
+        private Piece _piece;
+        private bool _isSelected;
+        private bool _isHighlighted;
+        private static readonly SolidColorBrush LightSquare = new SolidColorBrush(Color.FromRgb(240, 217, 181));
+        private static readonly SolidColorBrush DarkSquare = new SolidColorBrush(Color.FromRgb(181, 136, 99));
+        private static readonly SolidColorBrush SelectedColor = new SolidColorBrush(Color.FromRgb(247, 247, 105));
+        private static readonly SolidColorBrush HighlightColor = new SolidColorBrush(Color.FromRgb(247, 247, 128));//105 ou 128
+
+        public (int Ligne, int Colonne) Position => (Ligne, Colonne);
+
+        public Piece Piece
+        {
+            get => _piece;
+            set
+            {
+                _piece = value;
+                OnPropertyChanged(nameof(Piece));
+                OnPropertyChanged(nameof(Symbole));
+                OnPropertyChanged(nameof(BackgroundColor));
+            }
+        }
+
+        public string Symbole => Piece?.Symbole ?? "";
+
+        public bool IsSelected
+        {
+            get => _isSelected;
+            set
+            {
+                if (_isSelected != value)
+                {
+                    _isSelected = value;
+                    OnPropertyChanged(nameof(IsSelected));
+                    OnPropertyChanged(nameof(BackgroundColor));
+                }
+            }
+        }
+
+        public bool IsHighlighted
+        {
+            get => _isHighlighted;
+            set
+            {
+                if (_isHighlighted != value)
+                {
+                    _isHighlighted = value;
+                    OnPropertyChanged(nameof(IsHighlighted));
+                    OnPropertyChanged(nameof(BackgroundColor));
+                }
+            }
+        }
+
+        public Brush BackgroundColor
+        {
+            get
+            {
+                if (IsSelected)
+                    return SelectedColor;
+                if (IsHighlighted)
+                    return HighlightColor;
+                return (Ligne + Colonne) % 2 == 0 ? LightSquare : DarkSquare;
+            }
+        }
+
+        public CaseViewModel(int ligne, int colonne, Piece piece)
+        {
+            Ligne = ligne;
+            Colonne = colonne;
+            Piece = piece;
+            IsSelected = false;
+            IsHighlighted = false;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
-
-    public string Symbole => Piece?.Symbole ?? "";
-
-    public CaseViewModel(int ligne, int colonne, Piece piece)
-    {
-      Ligne = ligne;
-      Colonne = colonne;
-      Piece = piece;
-    }
-
-    public event PropertyChangedEventHandler PropertyChanged;
-    protected void OnPropertyChanged(string prop)
-        => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
-  }
 }
--- FredChessGame/ViewModel/MainViewModel.cs
+++ FredChessGame/ViewModel/MainViewModel.cs
@@ -1,23 +1,136 @@
 ﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+using System.Windows;
+using FredChessGame.Model;
+using System.Linq;
 
 namespace FredChessGame.ViewModel
 {
-  public class MainViewModel
-  {
-    public ObservableCollection<CaseViewModel> Cases { get; }
-
-    public MainViewModel()
+    public class MainViewModel
     {
-      var plateau = new Plateau(); // ton modèle logique du jeu
-      Cases = new ObservableCollection<CaseViewModel>();
+        public ObservableCollection<CaseViewModel> Cases { get; }
+        private CaseViewModel _selectedCase;
+        private Plateau _plateau;
 
-      for (int row = 0; row < 8; row++)
-      {
-        for (int col = 0; col < 8; col++)
+        public ICommand CaseClickCommand { get; }
+
+        public MainViewModel()
         {
-          Cases.Add(new CaseViewModel(row, col, plateau.Cases[row, col]));
+            _plateau = new Plateau();
+            Cases = new ObservableCollection<CaseViewModel>();
+            InitializeBoard();
+            CaseClickCommand = new RelayCommand<CaseViewModel>(OnCaseClicked);
         }
-      }
+
+        private void InitializeBoard()
+        {
+            for (int row = 0; row < 8; row++)
+            {
+                for (int col = 0; col < 8; col++)
+                {
+                    Cases.Add(new CaseViewModel(row, col, _plateau.Cases[row, col]) 
+                    { 
+                        IsSelected = false,
+                        IsHighlighted = false
+                    });
+                }
+            }
+        }
+
+        private void OnCaseClicked(CaseViewModel clickedCase)
+        {
+            // Si aucune case n'est sélectionnée et qu'on clique sur une pièce
+            if (_selectedCase == null && clickedCase.Piece != null)
+            {
+                // Désélectionner toutes les cases
+                ClearSelections();
+                
+                // Sélectionner la case cliquée
+                _selectedCase = clickedCase;
+                _selectedCase.IsSelected = true;
+                
+                // Mettre en surbrillance les mouvements possibles
+                HighlightPossibleMoves(clickedCase);
+            }
+            // Si une case est déjà sélectionnée et qu'on clique sur une autre case
+            else if (_selectedCase != null)
+            {
+                // Si on clique sur une pièce de la même couleur, on change la sélection
+                if (clickedCase.Piece != null && 
+                    clickedCase.Piece.Couleur == _selectedCase.Piece?.Couleur)
+                {
+                    ClearSelections();
+                    _selectedCase = clickedCase;
+                    _selectedCase.IsSelected = true;
+                    HighlightPossibleMoves(clickedCase);
+                }
+                // Sinon, on tente de déplacer la pièce
+                else
+                {
+                    if (TryMovePiece(_selectedCase, clickedCase))
+                    {
+                        // Déplacement réussi
+                        ClearSelections();
+                    }
+                    else
+                    {
+                        // Déplacement invalide, on ne fait rien ou on affiche un message
+                        MessageBox.Show("Mouvement invalide !", "Erreur", 
+                                      MessageBoxButton.OK, MessageBoxImage.Warning);
+                    }
+                }
+            }
+        }
+
+        private bool TryMovePiece(CaseViewModel fromCase, CaseViewModel toCase)
+        {
+            // Vérifier si le déplacement est valide selon les règles du jeu
+            if (fromCase.Piece.PeutSeDeplacer(fromCase.Ligne, fromCase.Colonne, 
+                                            toCase.Ligne, toCase.Colonne))
+            {
+                // Vérifier s'il y a une pièce sur la case d'arrivée
+                if (toCase.Piece != null)
+                {
+                    // Vérifier que ce n'est pas une pièce de la même couleur
+                    if (toCase.Piece.Couleur == fromCase.Piece.Couleur)
+                        return false;
+                    
+                    // Manger la pièce adverse
+                    toCase.Piece = null;
+                }
+
+                // Déplacer la pièce
+                toCase.Piece = fromCase.Piece;
+                fromCase.Piece = null;
+                
+                return true;
+            }
+            
+            return false;
+        }
+
+        private void HighlightPossibleMoves(CaseViewModel selectedCase)
+        {
+            foreach (var caseVm in Cases)
+            {
+                caseVm.IsHighlighted = selectedCase.Piece.PeutSeDeplacer(
+                    selectedCase.Ligne, selectedCase.Colonne,
+                    caseVm.Ligne, caseVm.Colonne);
+            }
+        }
+
+        private void ClearSelections()
+        {
+            if (_selectedCase != null)
+                _selectedCase.IsSelected = false;
+                
+            foreach (var caseVm in Cases)
+            {
+                caseVm.IsSelected = false;
+                caseVm.IsHighlighted = false;
+            }
+            
+            _selectedCase = null;
+        }
     }
-  }
 }
