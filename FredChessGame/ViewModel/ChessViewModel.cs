1a0221260e4d1ac94f37e14733c7c49653c06c26
Freddy Juhel <domaine75@gmail.com>
2025-08-11 16:46:24 +0200

    add namespace to ChessViewModel class

--- FredChessGame/ViewModel/ChessViewModel.cs
+++ FredChessGame/ViewModel/ChessViewModel.cs
@@ -1,63 +1,67 @@
 ﻿using System;
-using System.Windows.Input;
 using System.ComponentModel;
-using FredChessGame;
+using System.Windows.Input;
 
-public class ChessViewModel: INotifyPropertyChanged
+namespace FredChessGame.ViewModel
 {
-  public Plateau Plateau { get; } = new Plateau();
-
-  private (int row, int col)? pieceSelectionnee = null;
-
-  public ICommand CaseCliqueeCommand { get; }
-
-  public ChessViewModel()
+  public class ChessViewModel: INotifyPropertyChanged
   {
-    CaseCliqueeCommand = new RelayCommand<object>(OnCaseCliquee);
-  }
+    public Plateau Plateau { get; } = new Plateau();
 
-  private void OnCaseCliquee(object param)
-  {
-    var position = param as Tuple<int, int>;
-    if (position == null)
+    private (int row, int col)? pieceSelectionnee = null;
+
+    public ICommand CaseCliqueeCommand { get; }
+
+    public ChessViewModel()
+    {
+      CaseCliqueeCommand = new RelayCommand<object>(OnCaseCliquee);
+    }
+
+    private void OnCaseCliquee(object param)
+    {
+      var position = param as Tuple<int, int>;
+      if (position == null)
+      {
         return;
-
-    int row = position.Item1;
-    int col = position.Item2;
-
-    var piece = Plateau.Cases[row, col];
-
-    // 1. Si aucune pièce sélectionnée
-    if (pieceSelectionnee == null)
-    {
-      // On peut sélectionner une pièce seulement si elle appartient au joueur actuel
-      if (piece != null && piece.Couleur == Plateau.JoueurActuel)
-      {
-        pieceSelectionnee = (row, col);
-        // Ici tu peux déclencher une notification pour surligner
-      }
-    }
-    else
-    {
-      var (selRow, selCol) = pieceSelectionnee.Value;
-      var pieceSelec = Plateau.Cases[selRow, selCol];
-
-      // Vérifier si le déplacement est valide
-      if (pieceSelec != null && pieceSelec.PeutSeDeplacer(selRow, selCol, row, col))
-      {
-        Plateau.Cases[row, col] = pieceSelec;
-        Plateau.Cases[selRow, selCol] = null;
-        Plateau.ChangerDeJoueur(); // Changer de joueur
       }
 
-      pieceSelectionnee = null;
+      int row = position.Item1;
+      int col = position.Item2;
 
-      // Notifier la mise à jour de l'UI
-      OnPropertyChanged(nameof(Plateau));
+      var piece = Plateau.Cases[row, col];
+
+      // 1. Si aucune pièce sélectionnée
+      if (pieceSelectionnee == null)
+      {
+        // On peut sélectionner une pièce seulement si elle appartient au joueur actuel
+        if (piece != null && piece.Couleur == Plateau.JoueurActuel)
+        {
+          pieceSelectionnee = (row, col);
+          // Ici tu peux déclencher une notification pour surligner
+        }
+      }
+      else
+      {
+        var (selRow, selCol) = pieceSelectionnee.Value;
+        var pieceSelec = Plateau.Cases[selRow, selCol];
+
+        // Vérifier si le déplacement est valide
+        if (pieceSelec != null && pieceSelec.PeutSeDeplacer(selRow, selCol, row, col))
+        {
+          Plateau.Cases[row, col] = pieceSelec;
+          Plateau.Cases[selRow, selCol] = null;
+          Plateau.ChangerDeJoueur(); // Changer de joueur
+        }
+
+        pieceSelectionnee = null;
+
+        // Notifier la mise à jour de l'UI
+        OnPropertyChanged(nameof(Plateau));
+      }
     }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    protected virtual void OnPropertyChanged(string propName)
+        => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
   }
-
-  public event PropertyChangedEventHandler PropertyChanged;
-  protected virtual void OnPropertyChanged(string propName)
-      => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
 }
